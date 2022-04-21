801295eab0448acb068be81f77f28ea9354dcb8d
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-04-20 20:22:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,117 +1,58 @@
 class GuessingTrivia {
   constructor(element, options = {}) {
-    this.useCategoryIds = options.useCategoryIds || [];
-    this.useCategoryIds = options.useCategoryIds[(255, 190, 135, 145, 130)];
-
+    this.categoryIds = [255, 190, 135, 145, 130];
     // 3 letter words 255, movies 130,food and drink 190, mythology 135, nonfinction 145
 
     /*fetch this from api */
-    this.categories = [];
-    this.clues = {};
-    this.currentClue = nulll;
-    // Form
-    this.formElement = element.querySelector(".form");
-    this.gameBoardElement = element.querySelector(".game-board");
-    this.scoreCountElement = element.querySelector(".score-count");
-    this.resultElement = element.querySelector(".results");
-    this.resultTextElement = element.querySelector(
+    this.categories = { 255: [], 190: [], 135: [], 145: [], 130: [] };
+
+    // Form elements
+    this.formElement = document.querySelector(".form");
+    this.gameBoardElement = document.querySelector(".table");
+    this.scoreCountElement = document.querySelector(".score-count");
+    this.resultElement = document.querySelector(".results");
+    this.resultTextElement = document.querySelector(
       ".result_correct-answer-text"
     );
-    this.successTextElement = element.querySelector(".result_success");
-    this.failTextElement = element.querySelector(".result_fail");
+    this.successTextElement = document.querySelector(".result_success");
+    this.failTextElement = document.querySelector(".result_fail");
   }
+
+  async fetchDataByCategory(id) {
+    const response = await fetch(`https://jservice.io/api/category?id=${id}`);
+    const data = await response.json();
+    const clues = data.clues;
+    this.categories[id] = [clues[0], clues[1], clues[2], clues[3], clues[4]];
+    console.log(this.categories);
+  }
+
+  getQuestionsByCategory(id) {
+    const data = this.categories[id];
+    return data.map((singleObject) => singleObject.question);
+  }
+
   //score update on DOM
   initGame() {
-    this.updateScore();
-    this.fetchCategories();
-  }
-
-  updateScore(change) {
-    this.score += change;
-    this.scoreCountElement.textContent = this.score;
-  }
-
-  fetchCategories() {
-    const categories = this.useCategoryIds.map((categoryId) => {
-      return new Promise((resolve, reject) => {
-        fetch(`https://jservice.io/api/category?id=${categoryId}`).then(
-          (response) =>
-            response.json().then((data) => {
-              //console.log(data)
-              resolve(data);
-            })
-        );
-      });
-    });
-
-    //defines the table
-    let table = document.getElementById("table1");
-
-    alert(table.rows.length); // number of rows
-    alert(table.rows[5].cells.length); // number of cells in row 5
-
-    alert(table.rows[5].cells[4].innerHTML); // contents of 6th cell in 3rd row
-
-    Promise.all(categories).then((results) => {
-      results.forEach((category, categoryIndex) => {
-        var newCategory = {
-          title: category.title,
-          clues: [],
-        };
-
-        let clues = shuffle(results.clues)
-          .splice(0, 5)
-          .forEach((clue, index) => {
-            let clueId = categoryIndex + "-" + index;
-            newCategory.clues.push(clueId);
-
-            this.clues[clueId] = {
-              question: clue.question,
-              answer: clue.answer,
-              value: (index + 1) * 100,
-            };
-          });
-        this.categories.push(newCategory);
-      });
-      //  render each category in each column
-      this.categories.forEach((c) => {
-        this.renderCategories(c);
-      });
-    });
-  }
-  renderCategories(category) {
-    let column = document.createElement("table");
-    column.classList.add("column");
+    this.categoryIds.forEach((category) => this.fetchDataByCategory(category));
+    // this.fetchCategories();
   }
 }
 
-let questionCard = window.open("", )
-
-
-
-
-
-// event listeners
-this.gameBoardElement.addEventListener("click", (event) => {
-  if (event.target.dataset.clueId) {
-    this.handleClueClick(event);
-  }
-});
-this.formElement.addEventListener("submit", (event) => {
-  this.handleFormSubmit(event);
-});
-
-//shuffle the array
-function shuffle(a) {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    x = a[i];
-    a[i] = a[j];
-    a[j] = x;
-  }
-  return a;
-}
-
-const game = new JeopardyGame(document.querySelector(".app"), {});
+const game = new GuessingTrivia(document.querySelector(".app"), {});
 game.initGame();
+
+const table = document.querySelector("table");
+const tableBtns = table.querySelectorAll("button");
+console.log(tableBtns);
+tableBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const category = e.target.dataset.category;
+    const id = e.target.id;
+    const question = game.getQuestionsByCategory(category)[Number(id) - 1];
+    console.log(question);
+  });
+});
+
+//Show the result container
+const modal = document.querySelector(".question-card-modal");
+/*modal.classList.add("showing-result");*/
