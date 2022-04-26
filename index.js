f02dcf0da8e8087969e14f0e1ce8ad6994205674
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-04-25 22:45:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 class GuessingTrivia {
   constructor(element, options = {}) {
     this.categoryIds = [255, 190, 135, 145, 130];
+    // Default Categories pulled from https://jservice.io/search:
     // 3 letter words 255, movies 130,food and drink 190, mythology 135, nonfinction 145
 
     /*fetch this from api */
@@ -16,7 +17,9 @@ class GuessingTrivia {
     );
     this.successTextElement = document.querySelector(".result_success");
     this.failTextElement = document.querySelector(".result_fail");
+    this.answer = document.querySelector("input[name=user-answer]");
   }
+
 
   async fetchDataByCategory(id) {
     const response = await fetch(`https://jservice.io/api/category?id=${id}`);
@@ -53,48 +56,95 @@ tableBtns.forEach((btn) => {
   });
 });
 
+ //Create unique ID for this clue
+ const clueId = categoryIndex + "-" + index;
+ category.clues.push(clueId);
 
+ //Add clue to DB
+this.clues[clueId] = {
+question: clue.question,
+answer: clue.answer,
+value: (index + 1) * 100
+};
+         
+const tr = document.querySelector("#tr")
+const trChildrens = Array.from(tr.children)
+
+trChildrens.forEach(child => {
+  child.addEventListener("click", () => {
+    console.log("Hey, I've been clicked!.")
+  })
+})
+
+
+const modal_container = document.getElementById('modal-container');
+trChildrens.addEventListener('click', () => {
+    modal_container.classList.show('modal-container');
+});
+
+
+
+
+
+
+/*
 // Form pop up 
 
-this.boardElement.addEventListener("click", event => {
-    if (event.target.dataset.clueId) {
-       this.handleClueClick(event);
-    }
- });
- this.formElement.addEventListener("submit", event => {
-    this.handleFormSubmit(event);
- });
-
-
- function handleClueClick(event) {
-     console.log ("Button Clicked")
- }
- const btn = document.querySelectorAll('.btn');
-btn.addEventListener('click', handleClueClick);
-
- handleClueClick(onclick) {
-    var clue = this.clues[event.target.dataset.clueId];
-
-   
-handleFormSubmit(onclick) {
+function handleFormSubmit(onclick) {
+    document.getElementById("form").tableBtns 
     onclick.preventDefault();
     var isCorrect = this.cleanseAnswer(this.inputElement.value) === this.cleanseAnswer(this.currentClue.answer);
       if (isCorrect) {
          this.updateScore(this.currentClue.value);
       }
+    }
+
+    function handleClueClick(onclick) {
+        var clue = this.clues[onclick.target.dataset.clueId];
+  
+        
+        onclick.target.classList.add("used");
+        this.inputElement.value = "";
+        this.currentClue = clue;
+        //Update the text
+        this.clueTextElement.textContent = this.currentClue.question;
+        this.resultTextElement.textContent = this.currentClue.answer;
+  
+        //Hide the result
+        this.modalElement.classList.remove("showing-result");
+        
+        //Show the modal
+        this.modalElement.classList.add("visible");
+        this.answerElement.focus();
+    
+  
+     //Handle an answer from user
+     handleFormSubmit(onclick) {
+        onclick.preventDefault();
+
+        //Handle an answer from user
+   handleFormSubmit(event) {
+    event.preventDefault();
+    
+    var isCorrect = this.cleanseAnswer(this.inputElement.value) === this.cleanseAnswer(this.currentClue.answer);
+    if (isCorrect) {
+
+        this.updateScore(this.currentClue.value);
+      }
 
       //Show answer
       this.revealAnswer(isCorrect);
    }
-}\
-
-   onclick.target.classList.add("used");
-
-   //Clear out the input field
-   this.inputElement.value = "";
+   
+     }
+    }
 
 
-   //Hide the result
-this.modalElement.classList.remove("showing-result");
-this.modalElement.classList.add("visible");
-      this.inputElement.focus();
+//SCORE
+updateScore(change) {
+    this.score = [0];
+    this.score += change;
+    this.scoreCountElement.textContent = this.score;
+ }
+ */
+ 
